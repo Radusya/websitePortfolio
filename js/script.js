e393a16194aa2752687391d59dc11d0fab9a43dd
radusya <radulya777@icloud.com>
2023-01-12 21:01:56 +0100

    contact form upgrade

--- js/script.js
+++ js/script.js
@@ -10,33 +10,41 @@ function viewDiv(){
     
   };
 
+  function closeModal(){
+    document.querySelector('.modal').style.display = "none";
+  };
+
   function validateForm() {
     var name =  document.getElementById('name').value;
     if (name == "") {
-        document.querySelector('.status').innerHTML = "Name cannot be empty";
+        document.querySelector('.name').innerHTML = "Name cannot be empty";
+        document.querySelector('.name').style.color = "#FF0000";
         return false;
     }
     var email =  document.getElementById('email').value;
     if (email == "") {
-        document.querySelector('.status').innerHTML = "Email cannot be empty";
+        document.querySelector('.mail').innerHTML = "Email cannot be empty";
+        document.querySelector('.mail').style.color = "#FF0000";
         return false;
     } else {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if(!re.test(email)){
-            document.querySelector('.status').innerHTML = "Email format invalid";
+            document.querySelector('.mail').innerHTML = "Email format invalid";
+            document.querySelector('.mail').style.color = "#FF0000";
             return false;
         }
     }
     var subject =  document.getElementById('subject').value;
     if (subject == "") {
-        document.querySelector('.status').innerHTML = "Subject cannot be empty";
+        document.querySelector('.subject').innerHTML = "Subject cannot be empty";
+        document.querySelector('.subject').style.color = "#FF0000";
         return false;
     }
     var message =  document.getElementById('message').value;
     if (message == "") {
-        document.querySelector('.status').innerHTML = "Message cannot be empty";
+        document.querySelector('.mes').innerHTML = "Message cannot be empty";
+        document.querySelector('.mes').style.color = "#FF0000";
         return false;
     }
-    document.querySelector('.status').innerHTML = "Thanks, I received your mail!";
   };
 
