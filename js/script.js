b07b8c66112d8f73c35776c0d49bed9c95907652
radusya <radulya777@icloud.com>
2023-01-17 23:40:47 +0100

    pricing added v1

--- js/script.js
+++ js/script.js
@@ -1,46 +1,51 @@
-function viewDiv(){
+function viewDiv() {
     const button = document.getElementById('but');
-    if(document.getElementById("divmore").style.display == "none"){
+    if (document.getElementById("divmore").style.display == "none") {
         document.getElementById("divmore").style.display = "block";
         button.innerText = "↑";
     } else {
         document.getElementById("divmore").style.display = "none";
         button.innerText = "↓";
     }
-    
-  };
 
-  function validateForm() {
-    var name =  document.getElementById('name').value;
+};
+
+function validateForm() {
+    var name = document.getElementById('name').value;
     if (name == "") {
         document.querySelector('.name').innerHTML = "Name cannot be empty";
         document.querySelector('.name').style.color = "#FF0000";
         return false;
     }
-    var email =  document.getElementById('email').value;
+    var email = document.getElementById('email').value;
     if (email == "") {
         document.querySelector('.mail').innerHTML = "Email cannot be empty";
         document.querySelector('.mail').style.color = "#FF0000";
         return false;
     } else {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(!re.test(email)){
+        if (!re.test(email)) {
             document.querySelector('.mail').innerHTML = "Email format invalid";
             document.querySelector('.mail').style.color = "#FF0000";
             return false;
         }
     }
-    var subject =  document.getElementById('subject').value;
+    var subject = document.getElementById('subject').value;
     if (subject == "") {
         document.querySelector('.subject').innerHTML = "Subject cannot be empty";
         document.querySelector('.subject').style.color = "#FF0000";
         return false;
     }
-    var message =  document.getElementById('message').value;
+    var message = document.getElementById('message').value;
     if (message == "") {
         document.querySelector('.mes').innerHTML = "Message cannot be empty";
         document.querySelector('.mes').style.color = "#FF0000";
         return false;
     }
-  };
+};
 
+function RangeMonth() {
+    var val = parseInt(document.getElementById('customRange3').value);
+    document.getElementById('rangeval').innerText = `${val + 1} month`;
+    if (val != 0) document.getElementById('rangeval').innerText += `s`;
+};
